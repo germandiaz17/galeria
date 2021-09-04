38e0b3daac955830a1cae1e7628f7b61ffa1985a
German Diaz <germanrodrigo.2003@hotmail.com>
2021-09-03 19:02:44 -0500

    finalizado

---
+++ src/components/PageGrid.jsx
@@ -0,0 +1,33 @@
+//STYLES
+import '../styles/PageGrid.css'
+
+const PageGrid = () => {
+    return ( 
+        <div className='content'>
+            <div className='header'>
+                <h1>galeria.</h1>
+                <h5>START SLIDESHOW</h5>
+            </div>
+            <hr />
+            <div class="wrapper">
+                <div className="one">One</div>
+                <div className="two">Two</div>
+                <div className="three">Three</div>
+                <div className="four">Four</div>
+                <div className="five">Five</div>
+                <div className="six">Six</div>
+                <div className="seven">Seven</div>
+                <div className="eight">Eight</div>
+                <div className="nine">Nine</div>
+                <div className="ten">Ten</div>
+                <div className="eleven">Eleven</div>
+                <div className="twelve">Twelve</div>
+                <div className="thirteen">Thirteen</div>
+                <div className="fourteen">Fourteen</div>
+                <div className="fiveteen">Fiveteen</div>
+            </div>
+        </div>
+     );
+}
+ 
+export default PageGrid;
